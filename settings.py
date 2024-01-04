412a98366b9f2b59f7b2426f4cf673faaafefd9b
Willy <r10323039@ntu.edu.tw>
2024-01-04 21:14:19 +0800

    default test2

--- settings.py
+++ settings.py
@@ -1,18 +1,18 @@
 from os import environ
 
 SESSION_CONFIGS = [
-     {
-        'name': 'test',
-        'display_name': 'test',
-        'app_sequence': ['test'],
-        'num_demo_participants': 12,
-   
-    },
-     {
+    {
         'name': 'test2',
         'display_name': 'test2',
         'app_sequence': ['test2'],
         'num_demo_participants': 4,
+   
+    },
+    {
+        'name': 'test',
+        'display_name': 'test',
+        'app_sequence': ['test'],
+        'num_demo_participants': 6,
    
     }
 ]
@@ -20,11 +20,11 @@ SESSION_CONFIGS = [
 SESSION_CONFIG_DEFAULTS = {
     'name': 'My Default Session',
     'display_name': 'Default Session',
-    'num_demo_participants': 6,
+    'num_demo_participants': 4,
     'real_world_currency_per_point': 1/7,
     'name': 'sd-cb',
     'display_name': "sd-cb",
-    'num_demo_participants': 6,
+    'num_demo_participants': 4,
     'use_firm_belief_elicitation': True,
     'use_worker_belief_elicitation': True,
     'num_first_stage_rounds': 20,
